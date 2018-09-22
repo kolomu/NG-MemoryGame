5b2aa76735548f9ab99c01a9d14ef9ea15422252
kolomu <kevin-olomu@web.de>
2018-09-22 15:55:32 +0200

    added some more cards, added assets to gitignore, included bootstrap

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -8,7 +8,7 @@ import { CardService } from '../card.service';
   styleUrls: ['./card-list.component.scss']
 })
 export class CardListComponent implements OnInit {
-  readonly frontImagePath = '../assets/front.jpg';
+  readonly frontImagePath = '../assets/front.png';
   cards: Card[] = [];
   activeCard1: Card;
   activeCard2: Card;
@@ -16,9 +16,16 @@ export class CardListComponent implements OnInit {
 
   constructor(private cardService: CardService) {
     try {
-      this.cardService.createCard(new Card(1, this.frontImagePath, '../assets/back1.jpg'));
-      this.cardService.createCard(new Card(2, this.frontImagePath, '../assets/back2.jpg'));
-      this.cardService.createCard(new Card(3, this.frontImagePath, '../assets/back3.jpg'));
+      this.cardService.createCard(new Card(1, this.frontImagePath, '../assets/back1.png'));
+      this.cardService.createCard(new Card(2, this.frontImagePath, '../assets/back2.png'));
+      this.cardService.createCard(new Card(3, this.frontImagePath, '../assets/back3.png'));
+      this.cardService.createCard(new Card(4, this.frontImagePath, '../assets/back4.png'));
+      this.cardService.createCard(new Card(5, this.frontImagePath, '../assets/back5.png'));
+      this.cardService.createCard(new Card(6, this.frontImagePath, '../assets/back6.png'));
+      this.cardService.createCard(new Card(7, this.frontImagePath, '../assets/back7.png'));
+      this.cardService.createCard(new Card(8, this.frontImagePath, '../assets/back8.png'));
+      this.cardService.createCard(new Card(9, this.frontImagePath, '../assets/back9.png'));
+      this.cardService.createCard(new Card(10, this.frontImagePath, '../assets/back10.png'));
       this.cards = this.cardService.cards;
     } catch (err) {
       console.log('Some error happened while creating the cards...');
