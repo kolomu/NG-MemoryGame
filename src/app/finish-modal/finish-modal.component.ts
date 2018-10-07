9d8ce8ca5930ca0fdf971d5b5ded068eb26d8e82
kolomu <kevin-olomu@web.de>
2018-10-07 13:34:52 +0200

    Points with penality

--- src/app/finish-modal/finish-modal.component.ts
+++ src/app/finish-modal/finish-modal.component.ts
@@ -6,12 +6,48 @@ import { Component, Input } from '@angular/core';
   <h2>{{result ? 'Victory' : 'Defeat'}}</h2>
   <span>Time: {{time}}</span>
   <span *ngIf="skill">Skill: {{skill}}</span>
+  <span *ngIf="result">Points: {{getPoints()}}</span>
   `
 })
 export class FinishModalComponent {
-
   @Input() result: boolean;
   @Input() time: number;
   @Input() skill: number;
+  _finalTime = 0;
 
+  getPoints() {
+    if (this.skill < 100) {
+      this._finalTime = this.getPenality();
+    }
+
+    this._finalTime += this.time;
+
+    return this._finalTime;
+  }
+
+  private getPenality(): number {
+    if (this.skill < 20) {
+      return 10000;
+    }
+
+    if (this.skill < 40) {
+      return 5000;
+    }
+
+    if (this.skill < 60) {
+      return 3000;
+    }
+
+    if (this.skill < 80) {
+      return 1500;
+    }
+
+    if (this.skill < 90) {
+      return 1000;
+    }
+
+    if (this.skill < 100) {
+      return 500;
+    }
+  }
 }
